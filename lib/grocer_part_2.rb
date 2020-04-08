9b6fd7c37bbb0820ef578ed341369f0b8d36aaa8
Adao Natalino <sansonov@gmail.com>
2020-04-08 15:37:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,10 +4,10 @@ require 'pry'
 def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length
-    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
-    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-    if cart_item && cart_item[:count] >= coupons[counter][:num]
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart) #finding if the cart have cupons to apply
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON" #changing the name to what the new array should be
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart) 
+    if cart_item && cart_item[:count] >= coupons[counter][:num] #check if cart has enough itens to apply coupon
       if cart_item_with_coupon
         cart_item_with_coupon[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
